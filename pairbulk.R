43fc0922a573d5bd67514ffd0cd2925293c226ee
sewardlee337 <sewardlee337@gmail.com>
2017-06-12 00:06:12 -0700

    Create function for POST requests

--- pairbulk.R
+++ pairbulk.R
@@ -1,6 +1,5 @@
 ###  This .R file makes API calls to the USPTO's PAIR Bulk Data API
 
-
 library(jsonlite)
 library(httr)
 library(dplyr)
@@ -8,14 +7,33 @@ library(dplyr)
 pair.bulk.url <- 'https://pairbulkdata.uspto.gov/api/'  # This is the root URL for API
 
 
-# Return schema fields, as well as frequency and sample values
+##   Function to return search fields for API queries
 
 GetFieldsPAIR <- function(){
-     search.fields <- pair.bulk.url %>%
+     fields <- pair.bulk.url %>%
           paste0('/search-fields') %>%
-          GET() %>%
-          content()
+          httr::GET() %>%
+          httr::content()
      
-     return(search.fields)
+     return(fields)
 }
 
+
+GetFieldsPAIR()     ## Test to see if it works
+
+##   Function to make a POST request
+
+PostPAIR <- function(search.text, query.fields) {
+     
+     query.request <- list(searchText = search.text, qf = query.fields)
+     
+     response <- pair.bulk.url %>%
+          paste0('/queries') %>%
+          httr::POST(body = query.request, encode ='json') %>%
+          httr::content()
+     
+     return(response)
+}
+
+
+PostPAIR('diabetes', 'patentTitle')     # Test using arguments from API documentation   
