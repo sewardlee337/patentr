6aa73380bde72146d18571cb0b5a577f55e710c7
sewardlee337 <sewardlee337@gmail.com>
2017-06-11 22:50:30 -0700

    Refactor code to call search fields to function

--- pairbulk.R
+++ pairbulk.R
@@ -8,26 +8,14 @@ library(dplyr)
 pair.bulk.url <- 'https://pairbulkdata.uspto.gov/api/'  # This is the root URL for API
 
 
-# Return links to resources available in HATEOAS style
-
-resources <- pair.bulk.url %>%
-    GET() 
-
-http_type(resources)    # data format
-print(resources)        # return R list
-str(resources)          # see structure of list
-
-
 # Return schema fields, as well as frequency and sample values
 
-search.fields <- pair.bulk.url %>%
-    modify_url(path = '/search-fields') %>%
-    GET()
-
-http_type(search.fields)    # data format
-print(search.fields)        # return R list
-str(search.fields)          # see structure of list
-
-
-
+GetFieldsPAIR <- function(){
+     search.fields <- pair.bulk.url %>%
+          paste0('/search-fields') %>%
+          GET() %>%
+          content()
+     
+     return(search.fields)
+}
 
